6bb641c989a6e842a7f5a8a7ba7221c82d8dd4a1
Kattholl <katia161718@gmail.com>
2024-08-07 21:49:46 +0300

    T9

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static boolean greadYear(int year) {
+        if (year % 4==0){
+            return true;
+        } else if (year % 100==0){
+            return false;
+        } else {
+            return year % 400==0;
         }
+    }
+    public static void main(String[] args) {
+        System.out.println("2000 год: " + greadYear(2000));
+        System.out.println("2001 год: " + greadYear(2001));
+        System.out.println("2003 год: " + greadYear(2003));
+        System.out.println("2008 год: " + greadYear(2008));
     }
 }
